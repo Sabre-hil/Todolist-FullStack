398be29f403fb0e5c81bb15dded2946ef776760c
Хизир Абдеев <abdeevhizir95@gmail.com>
2023-02-08 18:27:26 +0300

    Update getRegThunk.ts

--- client/src/redux/Thunks/authThunks/getRegThunk.ts
+++ client/src/redux/Thunks/authThunks/getRegThunk.ts
@@ -10,25 +10,20 @@ interface regType {
 export const regThunk = createAsyncThunk(
   'auth/regThunk',
   async function (data: regType, {rejectWithValue}) {
-    try {
       const responce = await fetch('http://localhost:3005/registration', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',
         body: JSON.stringify(data)
       });
-      if (responce.ok) {
+      try {
         const data = await responce.json();
+        if (!responce.ok) {
+          throw new Error("Server Error!");
+        }
         return data;
-      } else {
-        alert('Произошла ошибка')
+      } catch (error: any) {
+        return rejectWithValue(error.message);
       }
-    } catch (error) {
-      let errorMessage = "Failed to do something exceptional";
-      if (error instanceof Error) {
-        errorMessage = error.message;
-      }
-      return rejectWithValue(errorMessage);
-    }
   }
 )
