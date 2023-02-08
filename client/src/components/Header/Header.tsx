fff843c02c63fe682b2f23c71be5049a392f8e70
Хизир Абдеев <abdeevhizir95@gmail.com>
2023-02-08 18:24:44 +0300

    fixed Header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -8,7 +8,7 @@ import { RootState, AppDispatch } from '../../redux/store';
 import { logoutUserThunk } from '../../redux/Thunks/authThunks/logoutUserThunk';
 
 const Header: React.FC = () => {
-  const { regAndAuthState } = useSelector((state: RootState) => state.auth);
+  const { regAndAuthState, status } = useSelector((state: RootState) => state.auth);
   const dispatch = useDispatch<AppDispatch>();
   const isMounted = React.useRef(false);
 
@@ -27,22 +27,22 @@ const Header: React.FC = () => {
   };
   return (
     <div className={styles.header}>
-      {!regAndAuthState ? (
+      {regAndAuthState && status === 'success'? (
+        <div className={styles.headerBlock}>
+        <div className={styles.item}><Link to="/"><img className={styles.logo} src={logo} alt="logo" /></Link></div>
+        <div><h2 className={styles.greetingsUser}>{`Привет ${regAndAuthState.name}`}</h2></div>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'baseline'}}>
+          <button className={styles.button} onClick={logout} type="button">Выйти</button>
+          <Sort />
+        </div>
+      </div>
+      ) : (
         <div className={styles.headerBlock}>
           <div className={styles.headerBlock}>
             <div className={styles.item}><Link to="/"><img className={styles.logo} src={logo} alt="logo" /></Link></div>
             <div className={styles.item}><Link className={styles.link} to="/regestration">Зарегестрироваться</Link></div>
             <div className={styles.item}><Link className={styles.link} to="/login">Авторизироваться</Link></div>
             <div className={styles.item}><Link className={styles.link} to="/">На главную страницу</Link></div>
-          </div>
-        </div>
-      ) : (
-        <div className={styles.headerBlock}>
-          <div className={styles.item}><Link to="/"><img className={styles.logo} src={logo} alt="logo" /></Link></div>
-          <div><h2 className={styles.greetingsUser}>{`Привет ${regAndAuthState.name}`}</h2></div>
-          <div style={{display: 'flex', justifyContent: 'center', alignItems: 'baseline'}}>
-            <button className={styles.button} onClick={logout} type="button">Выйти</button>
-            <Sort />
           </div>
         </div>
       )}
